e31bee4102365279f821ac31a9b4be0624de19aa
Eoin Nugent <enugent@hmc.edu>
2018-10-13 09:16:30 -0700

    Minor comments, some updated setup.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React from 'react';
-import { renderToString } from 'react-dom/server';
+import {renderToString} from 'react-dom/server';
 import htmlTemplate from './template';
 
 // [START gae_node_request_example]
@@ -12,6 +12,10 @@ const app = express();
 app.get('/', (req, res) => {
   // res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.send(htmlTemplate('hi!')).end();
+});
+
+app.get('/ajax', (req, res) => {
+  res.send(JSON.stringify({hello: 'there'})).end();
 });
 
 // Start the server
