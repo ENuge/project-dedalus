ccf7dea6c8d524d4f97ffcea45ce6be75bc84dfc
Eoin Nugent <enugent@hmc.edu>
2018-10-19 21:08:06 -0700

    Strava activities now read and write through to a cache, limiting the number of requests sent to Strava.

--- src/app/strava.jsx
+++ src/app/strava.jsx
@@ -4,15 +4,6 @@ import strava from 'strava-v3';
 import fs from 'fs';
 import type {$Request, $Response} from 'express';
 
-// Get data from strava using my API key (read from a local file
-// that is not committed to git). Post that, for now, in just a
-// raw format. Eventually I want it to be formatted with the ability
-// to easily add comments to workouts but we'll work our way to that!
-
-// POST: Allows me to change the name and description of the activity.
-// (Description can be useful for summarizing workouts - especially
-// if client can prefill based on the type of activity done.)
-
 const getCachedStrava = async (): ?Object => {
   const readFile = util.promisify(fs.readFile);
   let fileResponse = null;
@@ -36,10 +27,9 @@ const fetchActivitiesWithDetails = async (basicActivities: Object) => {
   );
 };
 
-// listOfActivities.reduce((activitiesAccum, activity) => {
-//   [...activitiesAccum, activity];
-// }, []);
-const mergeActivities = listsOfActivities => {
+const mergeActivities = (listsOfActivities: Array<Array<Object>>): Array<Object> => {
+  // Merges two _lists_ of activities, keeping the most descriptive copy of each
+  // activity.
   const activities = listsOfActivities.reduce(
     (listActivitiesAccum, listOfActivities) => [...listActivitiesAccum, ...listOfActivities],
     []
@@ -59,46 +49,54 @@ const mergeActivities = listsOfActivities => {
   }, []);
 };
 
+const cacheActivities = (activities): void => {
+  fs.writeFile('./cached/strava', JSON.stringify(activities), err => {
+    if (err) {
+      console.log(`Error writing to file! ${JSON.stringify(err)}`);
+    }
+  });
+};
+
 export const getStrava = async (req: $Request, res: $Response) => {
   const cachedStrava = await getCachedStrava();
   // If no cachedStrava, hit Strava's endpoint, get all activities with all
   // descriptions.
   const listActivitiesPromise = util.promisify(strava.athlete.listActivities);
   const basicActivitiesFromStrava = await listActivitiesPromise({});
-  const activitiesWithDetails =
-    cachedStrava || (await fetchActivitiesWithDetails(basicActivitiesFromStrava));
 
   // cachedStrava contains all activities with descriptions, but not necessarily
-  // the latest activities - in that case, meerge it with the data we just received
+  // the latest activities - in that case, merge it with the data we just received
   // from Strava.
-  const mergedActivities = cachedStrava
+  const activitiesWithDetails = cachedStrava
     ? mergeActivities([cachedStrava, basicActivitiesFromStrava])
-    : activitiesWithDetails;
+    : await fetchActivitiesWithDetails(basicActivitiesFromStrava);
 
-  // write mergedActivities to cache
-  // return mergedActivities
-  res.send(mergedActivities);
-
-  // strava.athlete.listActivities({}, (err, payload) => {
-  //   if (!err) {
-  //     res.send(payload);
-  //   } else {
-  //     console.log(`Error receiving response from strava: ${err}`);
-  //   }
-  // });
+  cacheActivities(activitiesWithDetails);
+  res.send(activitiesWithDetails);
 };
-export const postStrava = (req: $Request, res: $Response) => {
-  strava.activities.update({id: req.body.id, description: req.body.description}, (err, payload) => {
-    if (err) {
-      console.log(`Error updating description in Strava: ${JSON.stringify(err)}`);
-      res.send({success: false});
-    } else {
-      console.log(`Updated Strava activity to: ${JSON.stringify(payload)}`);
-      res.send({success: true});
+export const postStrava = async (req: $Request, res: $Response) => {
+  // Post the updated description to Strava.
+  // But first, merge it with our existing cached Strava activities
+  // so subsequent gets have that description.
+  const postedActivity = req.body;
+  // This cache should always be populated by now. But just in case,
+  // and to appease the flow gods, we'll check before merging.
+  const cachedActivities = await getCachedStrava();
+  const mergedActivities = cachedActivities
+    ? mergeActivities([cachedActivities, [postedActivity]])
+    : [postedActivity];
+  cacheActivities(mergedActivities);
+
+  strava.activities.update(
+    {id: postedActivity.id, description: postedActivity.description},
+    (err, payload) => {
+      if (err) {
+        console.log(`Error updating description in Strava: ${JSON.stringify(err)}`);
+        res.send({success: false});
+      } else {
+        console.log(`Updated Strava activity to: ${JSON.stringify(payload)}`);
+        res.send({success: true});
+      }
     }
-  });
+  );
 };
-
-// Add two things:
-// First, make getStrava read a cached copy. Get that, then add the latest from the Strava API hit. Update the cached copy and return the superset.
-// postStrava should update the cached copy with the POSTed data, then also post it to Strava.
