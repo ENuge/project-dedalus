eb8fdba00cf6fc3d65eef4fdc25b03334d7a7243
Eoin Nugent <enugent@hmc.edu>
2018-10-27 19:02:00 -0700

    Added an MIT-esque banner header and some initial css.

--- src/client/ReactBase.jsx
+++ src/client/ReactBase.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 import StaticQuote from './components/StaticQuote';
 import QuoteOfTheDay from './components/QuoteOfTheDay';
 import Strava from './components/Strava';
+import Header from './components/Header';
 
 const ReactBase = () => (
   <React.Fragment>
-    <StaticQuote />
-    <QuoteOfTheDay />
-    <Strava />
+    <Header />
+    <div className="content">
+      <StaticQuote />
+      <QuoteOfTheDay />
+      <Strava />
+    </div>
   </React.Fragment>
 );
 export default ReactBase;
---
+++ src/client/components/Header.jsx
@@ -0,0 +1,13 @@
+// @flow
+
+import React from 'react';
+
+const Header = () => (
+  <div id="header">
+    <div className="header-hero">
+      <div className="banner-text">Project Dedalus</div>
+    </div>
+  </div>
+);
+
+export default Header;
