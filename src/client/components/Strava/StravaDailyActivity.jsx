0b921a32de40b773d21419df39c9990e9e0af16a
Eoin Nugent <enugent@hmc.edu>
2018-11-02 23:02:32 -0700

    Set up initial date-picker and highlighted activity table for it, yay

--- src/client/components/Strava/StravaDailyActivity.jsx
+++ src/client/components/Strava/StravaDailyActivity.jsx
@@ -1,6 +1,7 @@
 // @flow
 import * as React from 'react';
 import type {Activities} from './Strava';
+import StravaTable from './StravaTable';
 
 const dateExtractorRegex = /^[0-9]{4}-[0-9]{2}-[0-9]{2}/;
 const millisecondsInDay = 1000 * 60 * 60 * 24; // he did the monster math
@@ -20,24 +21,24 @@ const getActivitiesForDate = (date: string, activities: Activities): Activities 
   // Assumes `date` is in the same format as Strava's.
   activities.filter(activity => isSameDate(activity.start_date, date));
 
+/**
+ * Returns a string formatted like YYYY-MM-DD with no time info.
+ * @param {*} dateString A string formatted like 2018-11-03T00:24:18Z
+ */
+const dateFromString = (dateString: string): string => {
+  const [date] = dateString.match(dateExtractorRegex) || [''];
+  return date;
+};
+
+// Returns an array of dates, without duplicates, that have at least one activity.
 const getUniqueActivityDates = (activities: Activities): Array<string> => {
-  const activityDates = activities.map(activity => activity.start_date);
+  const activityDates = activities.map(activity => dateFromString(activity.start_date));
   return Array.from(new Set([...activityDates]));
 };
 
 const dateStringRelativeToToday = (daysBack: number = 0): string => {
   const date = new Date(new Date() - millisecondsInDay * daysBack);
-  const [dateString] = date.toISOString().match(dateExtractorRegex) || [''];
-  return dateString;
-};
-
-const dateDiff = (dateString1: string, dateString2: string) => {
-  // Returns the difference in days between two date strings, as an
-  // absolute number.
-  const date1 = new Date(dateString1);
-  const date2 = new Date(dateString2);
-  const timeDiff = Math.abs(date2.getTime() - date1.getTime());
-  return Math.ceil(timeDiff / millisecondsInDay);
+  return dateFromString(date.toISOString());
 };
 
 const constructWeeklyDates = (initialDate: string, endDate: string): Array<Array<string>> => {
@@ -53,27 +54,27 @@ const constructWeeklyDates = (initialDate: string, endDate: string): Array<Array
   // column, unless the most recent date is a Sunday. (So the dates look
   // monotonically increasing, and you don't have a Monday in one column that falls
   // before the Wednesday in the given week.)
+  // We do this by comparing each day against a correction, which allows some days
+  // of the week to have an extra date.
   const endDay = new Date(endDate).getDay();
-  const correction = (dayOfWeek: number) => (endDay <= dayOfWeek ? 1 : 0);
+  const correction = (dayOfWeek: number) => (endDay >= dayOfWeek ? 1 : 0);
 
   let datesRemaining = true;
   let currDate = new Date(endDate);
   while (datesRemaining) {
     const dayOfWeek = currDate.getDay();
-    // Am I regretting using strings everywhere? Not at all...
-    const [dateString] = currDate.toISOString().match(dateExtractorRegex) || [''];
+    const dateString = dateFromString(currDate.toISOString());
     weeksWithDays[dayOfWeek].unshift(dateString);
 
-    // This is so terrible...
-    const mondayLength = weeksWithDays[0].length;
+    const mondayLength = weeksWithDays[0].length - correction(0);
     if (
       dateString <= initialDate &&
-      mondayLength === weeksWithDays[1].length + correction(1) &&
-      mondayLength === weeksWithDays[2].length + correction(2) &&
-      mondayLength === weeksWithDays[3].length + correction(3) &&
-      mondayLength === weeksWithDays[4].length + correction(4) &&
-      mondayLength === weeksWithDays[5].length + correction(5) &&
-      mondayLength === weeksWithDays[6].length + correction(6)
+      mondayLength === weeksWithDays[1].length - correction(1) &&
+      mondayLength === weeksWithDays[2].length - correction(2) &&
+      mondayLength === weeksWithDays[3].length - correction(3) &&
+      mondayLength === weeksWithDays[4].length - correction(4) &&
+      mondayLength === weeksWithDays[5].length - correction(5) &&
+      mondayLength === weeksWithDays[6].length - correction(6)
     ) {
       datesRemaining = false;
     }
@@ -82,6 +83,14 @@ const constructWeeklyDates = (initialDate: string, endDate: string): Array<Array
   }
 
   return weeksWithDays;
+};
+
+// Returns the date of the first activity or 30 days ago if no activities
+const getInitialDate = (activities: Activities) => {
+  const activityDates = getUniqueActivityDates(activities);
+  // Implicit sorting by Unicode code points gets dates in ascending order.
+  const datesByRecency = activityDates.sort();
+  return datesByRecency ? datesByRecency[0] : dateStringRelativeToToday(30);
 };
 
 type Props = {|activities: Activities|};
@@ -96,22 +105,20 @@ class StravaDailyActivity extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     const {activities} = this.props;
-    const activityDates = getUniqueActivityDates(activities);
     // Implicit sorting by Unicode code points gets dates in ascending order.
-    const datesByRecency = activityDates.sort();
-    const initialDate = datesByRecency ? datesByRecency[0] : dateStringRelativeToToday(30);
+    const initialDate = getInitialDate(activities);
     // const endDate = datesByRecency ? datesByRecency[0] : dateStringRelativeToToday();
     // NOTE: Think more about this - but I think I always want endDate for now
     // to be today, not relative to the data we have.
     const endDate = dateStringRelativeToToday();
     const focusedActivities = getActivitiesForDate(endDate, activities);
+    // debugger;
     this.state = {
       focusedDate: endDate,
       focusedActivities,
       initialDate,
       endDate,
     };
-    this.handleChangedFocusDate = this.handleChangedFocusDate.bind(this);
   }
 
   handleChangedFocusDate = (focusedDate: string) => {
@@ -121,25 +128,44 @@ class StravaDailyActivity extends React.Component<Props, State> {
   };
 
   render() {
-    // const {activities} = this.props;
-    const {initialDate, endDate} = this.state;
-    // const numWeeks = Math.ceil(numDaysInRange / 7);
-    // const totalDays = numWeeks * 7; // may be slightly larger than numDaysInRange
+    const {initialDate, endDate, focusedActivities, focusedDate} = this.state;
     // Each cell in our table corresponds to some day. Given it's a 7xX
     // table for some X number of weeks, construct all those dates
     const weeksWithDays = constructWeeklyDates(initialDate, endDate);
+    // use that to display activities to the right for that date.
+    // (Can just be as like a table or something to start.)
     return (
-      <table>
-        <tbody>
-          {weeksWithDays.map(week => (
-            <tr>
-              {week.map(day => (
-                <td>{day}</td>
+      <React.Fragment>
+        <div className="highlighted-date">
+          <table className="daily-calendar-table">
+            <tbody>
+              {weeksWithDays.map(week => (
+                <tr>
+                  {week.map(day => (
+                    <td>
+                      <div
+                        className="daily-activity-cell"
+                        onClick={() => this.handleChangedFocusDate(day)}
+                        role="link"
+                        tabIndex="0"
+                        onKeyPress={event =>
+                          event.keyCode === 13 ? this.handleChangedFocusDate(day) : null
+                        }
+                      >
+                        {day}
+                      </div>
+                    </td>
+                  ))}
+                </tr>
               ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+            </tbody>
+          </table>
+          <div className="highlighted-activities">
+            <h3>Activities on: {focusedDate}</h3>
+            <StravaTable activities={focusedActivities} onChange={() => {}} onSubmit={() => {}} />
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
