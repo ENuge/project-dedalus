32da4307b7fa5bce829539784436e7bfbabc57a4
Eoin Nugent <enugent@hmc.edu>
2019-05-12 22:40:06 +0100

    Forgot to add the actual about page content.

---
+++ src/client/components/site/WIPContent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const WIPContent = () => (
+  <div id="main-content">
+    <h1>I'm giving it all I've got, captain!</h1>
+    <p>Yeah, so…about those other pages you were going to write…</p>
+    <p>
+      It would be <em>great</em> if you could get them to me first thing tomorrow.
+    </p>
+    <p>
+      (These pages are a work in progress—hope to have something up for them soon! If they're like
+      this for quite a while, my contact details are on the About page—call me out.)
+    </p>
+  </div>
+);
+
+export default WIPContent;
